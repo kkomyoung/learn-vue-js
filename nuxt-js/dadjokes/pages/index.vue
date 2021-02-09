93d3a6e1ecea76017144d99e85cd8f5486efe509
Kim Younge Ae <jhy0088@naver.com>
2021-02-10 02:13:47 +0900

    update

---
+++ nuxt-js/dadjokes/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>
+      Welcome to the best corny dad jokes app on the web
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Welcom To Dad Jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ nuxt-js/dadjokes/pages/jokes/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>jokes</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      joke: []
+    }
+  },
+  async created() {
+    const config = {
+      headers: {
+        'Accept' : 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.get('https://icanhazdadjoke.com/search', config);
+
+      console.log(res.data);
+    } catch (err) {
+      console.log(err)
+    };
+
+    
+  },
+  head() {
+    return {
+      title: 'Dad Jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="">
+  
+</style>
