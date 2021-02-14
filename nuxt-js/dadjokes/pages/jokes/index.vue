cf5387d4598d072d7b8139cf2f08d5d1493d4442
Kim Younge Ae <jhy0088@naver.com>
2021-02-14 21:35:41 +0900

    update

--- nuxt-js/dadjokes/pages/jokes/index.vue
+++ nuxt-js/dadjokes/pages/jokes/index.vue
@@ -1,41 +1,63 @@
 <template>
-  <div>jokes</div>
+  <div>
+    <SearchJokes v-on:search-text="searchText"/>
+    <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke"/>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
-
+import axios from "axios";
+import Joke from "../../components/Joke";
+import SearchJokes from "../../components/SearchJokes";
 export default {
+  components: {
+    Joke,
+    SearchJokes
+  },
   data() {
     return {
-      joke: []
-    }
+      jokes: []
+    };
   },
   async created() {
     const config = {
       headers: {
-        'Accept' : 'application/json'
+        Accept: "application/json"
       }
     };
-
     try {
-      const res = await axios.get('https://icanhazdadjoke.com/search', config);
-
-      console.log(res.data);
+      const res = await axios.get("https://icanhazdadjoke.com/search", config);
+      this.jokes = res.data.results;
     } catch (err) {
-      console.log(err)
-    };
-
-    
+      console.log(err);
+    }
+  },
+  methods: {
+    async searchText(text) {
+      const config = {
+        headers: {
+          Accept: "application/json"
+        }
+      };
+      try {
+        const res = await axios.get(
+          `https://icanhazdadjoke.com/search?term=${text}`,
+          config
+        );
+        this.jokes = res.data.results;
+      } catch (err) {
+        console.log(err);
+      }
+    }
   },
   head() {
     return {
-      title: 'Dad Jokes',
+      title: "Dad Jokes",
       meta: [
         {
-          hid: 'description',
-          name: 'description',
-          content: 'Best place for corny dad jokes'
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes"
         }
       ]
     };
@@ -43,6 +65,5 @@ export default {
 };
 </script>
 
-<style lang="">
-  
+<style>
 </style>
